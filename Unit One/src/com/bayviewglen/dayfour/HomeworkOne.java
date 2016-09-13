a4ea4d585f106f917066822b13c244b77a88f716
jtran <jtran@jtransp3.bayviewglen.ca>
2016-09-13 19:04:21 -0400

    Day Four Work

---
+++ Unit One/src/com/bayviewglen/dayfour/HomeworkOne.java
@@ -0,0 +1,14 @@
+package com.bayviewglen.dayfour;
+
+public class HomeworkOne {
+
+	public static void main(String[] args) {
+		double x;
+		
+		x = 10;
+		
+		System.out.println("The number I chose is " +  Math.sqrt(x) + " .");
+
+	}
+
+}
