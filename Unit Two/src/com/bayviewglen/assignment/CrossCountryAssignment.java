18fecb46087e4055f7c0369ef687f5bec4e1373b
jtran <jtran@jtransp3.bayviewglen.ca>
2016-10-11 22:55:03 -0400

    TBC

--- Unit Two/src/com/bayviewglen/assignment/CrossCountryAssignment.java
+++ Unit Two/src/com/bayviewglen/assignment/CrossCountryAssignment.java
@@ -7,41 +7,48 @@ public class CrossCountryAssignment {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		final int SEC_TO_MINS = 60;
+		final int SIXTY = 60;
 		DecimalFormat formatter = new DecimalFormat("00.000");
-		
-				
-		// Runner 1
+
+		// Runner 1 
 		System.out.print("Please enter your first & last name: ");
-		String nameone = keyboard.nextLine(); 
+		String nameone = keyboard.nextLine();
 		String firstnameone = nameone.split(" ")[0];
 		String lastnameone = nameone.split(" ")[1];
-		
-		System.out.print(firstnameone + ", please enter your mile one time (MM:SS.SS):");
-		String onetimeone = keyboard.nextLine();
-		/*	
-		System.out.print("Please enter your time to mile two (MM:SS.SS):");
-		String twotimeone = keyboard.nextLine();
-		
-		System.out.print("Please enter your time to 5km (MM:SS.SS):");
-		String fulltimeone = keyboard.nextLine();
-		*/
-		
-		
-		System.out.println("Runner One Summary");
-		
-		//Split One
-		int x = Integer.parseInt(onetimeone.split(":")[0]);
-		double y = Double.parseDouble(onetimeone.split(":")[1]);
-		double z = (x*60) + y;
-		System.out.println(x + " " + y + " " + z);
-		
-		x = (int)(z/60);
-		y = z - (60*x);
-		String y2 = (formatter.format(y));
-		System.out.println(x + ":" + y2);
-		
-		
-	}
 
+		System.out.print(firstnameone + ", please enter your mile one time (MM:SS.SS):");
+		String runneronemiletimeone = keyboard.nextLine();
+		
+		System.out.print("Please enter your time to mile two (MM:SS.SS):");
+		String runneronemiletimetwo = keyboard.nextLine();
+		 
+		System.out.print("Please enter your time to complete 5km (MM:SS.SS):"); 
+		String runneronefivekmtime = keyboard.nextLine();
+		 
+
+
+		// Runner One Split One
+		int runnerOneSplitOne = Integer.parseInt(runneronemiletimeone.split(":")[0]);
+		double runnerOneSplitOneSeconds = Double.parseDouble(runneronemiletimeone.split(":")[1]);
+		double runnerOneSplitOneMinutes = (runnerOneSplitOneMinutes * SIXTY) + runnerOneSplitOneSeconds;
+		
+		runnerOneSplitOneMinutes = (int)-(runnerOneSplitOneTotal/SIXTY) ;
+		runnerOneSplitOneSeconds = runnerOneSplitOneTotal % SIXTY;
+		String SecondsFormatted = (formatter.format(runnerOneSplitOneSeconds));
+		String SplitFormatted = runnerOneSplitOneMinutes + ":" + SecondsFormatted; 
+		System.out.println("Time for first split: " );
+        
+		// Runner One Split Two
+		int runnerOneSplitTwo = Integer.parseInt(runneronemiletimetwo.split(":")[0]);
+		double runnerOneSplitTwoSeconds = Double.parseDouble(runneronemiletimetwo.split(":")[1]);
+		double runnerOneSplitTwoMinutes = (runnerOneSplitTwoMinutes * SIXTY) + runnerOneSplitTwoSeconds;
+	
+				
+		// Runner One Split Three
+				
+		// Runner One Summary
+		System.out.println(" ");
+		System.out.println("Runner One Summary");
+
+	}
 }
