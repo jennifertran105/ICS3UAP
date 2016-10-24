97e0455545dd0dadadf90602df8793a899c24532
jtran <jtran@jtransp3.bayviewglen.ca>
2016-10-24 14:08:43 -0400

    Unit Four Initial Commit

---
+++ Unit Four/src/com/bayviewglen/dayone/ExampleA.java
@@ -0,0 +1,16 @@
+package com.bayviewglen.dayone;
+
+public class ExampleA {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		int current = 1;
+		
+		while(current < 1001) {
+			
+		}
+
+	}
+
+}
