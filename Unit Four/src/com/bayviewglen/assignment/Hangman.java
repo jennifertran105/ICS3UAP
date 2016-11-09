9d4f9b6c93dd61a3cd12d0b8b57e85539d7161dd
jtran <jtran@jtransp3.bayviewglen.ca>
2016-11-09 14:57:08 -0500

    Work

--- Unit Four/src/com/bayviewglen/assignment/Hangman.java
+++ Unit Four/src/com/bayviewglen/assignment/Hangman.java
@@ -1,9 +1,23 @@
 package com.bayviewglen.assignment;
 
+import java.util.Scanner;
+
 public class Hangman {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+
+		final String VALID_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
+		final String VALID_CHARACTERS_NOSPACE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+		//Name Input
+		System.out.println("Player 1, please enter your name: ");
+		String namep1 = keyboard.nextLine();
+
+		System.out.println("Player 2, please enter your name: ");
+		String namep2 = keyboard.nextLine();
+		
+		//Round 
 
 	}
 
