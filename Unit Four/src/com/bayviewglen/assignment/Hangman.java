4695185b5180ad3b4bd08184635d2066614d6899
jtran <jtran@jtransp3.bayviewglen.ca>
2016-11-15 20:59:46 -0500

    ...

--- Unit Four/src/com/bayviewglen/assignment/Hangman.java
+++ Unit Four/src/com/bayviewglen/assignment/Hangman.java
@@ -8,17 +8,26 @@ public class Hangman {
 		Scanner keyboard = new Scanner(System.in);
 
 		final String VALID_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
-		final String VALID_CHARACTERS_NOSPACE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		final String VALID_CHARACTERS_NOSPACE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; 
+		final int MAX_POINTS = 8;
+		final int MAX_GUESSES = 7;
+		
 
-		//Name Input
-		System.out.println("Player 1, please enter your name: ");
+		// Name Input
+		System.out.print("Player 1, please enter your name: ");
 		String namep1 = keyboard.nextLine();
 
-		System.out.println("Player 2, please enter your name: ");
+		System.out.print("Player 2, please enter your name: ");
 		String namep2 = keyboard.nextLine();
-		
-		//Round 
 
+		System.out.println(namep1 + " and " + namep2 + " let's begin the game of Hangman...");
+
+		// Round
+			System.out.println("Round Start...");
+			System.out.println("Please enter a alphanumeric phase to guess:");
+			String phrase1 = keyboard.nextLine().toUpperCase();
+			
+			
+	
 	}
-
 }
